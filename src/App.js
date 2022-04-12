cd22df89fe0f1dab1b11f78c2f42d4c5451a3b65
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-04-12 21:41:06 +0200

    successful connection to firestore db

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.scss';
 import Header from './components/Header';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import TinderCards from './pages/TinderCards';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<h1>home</h1>}/>
+          <Route path="/" element={<TinderCards />}/>
           <Route path="/chat" element={<h1>chat</h1>}/>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/TinderCards.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import TinderCard from 'react-tinder-card'
+import collectionReference from '../firebase'
+import {doc, getDocs} from 'firebase/firestore'
+import './TinderCards.scss'
+function TinderCards() {
+    const [people, setPeople] = useState([])
+    //  [
+    //     {
+    //         name: 'steve jobs',
+    //         url: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTY2MzU3OTcxMTUwODQxNTM1/steve-jobs--david-paul-morrisbloomberg-via-getty-images.jpg'
+    //     },
+    //     {
+    //         name: 'bill gates',
+    //         url: 'https://ocdn.eu/pulscms-transforms/1/zs5k9kpTURBXy84NTVhYmM3MDk0YWEyYjEwNDQ1ODg2ZjYzOTA2ZjExZi5qcGeSlQM6Ls0Hls0ERZMFzQMUzQG8gqEwBaExAA'
+    //     }
+    // ]
+    useEffect(()=>{
+        getDocs(collectionReference)
+        .then((snapshot)=>{
+            setPeople(snapshot.docs.map(doc=>{
+                return {...doc.data(), id: doc.it}
+            }))
+        })
+        .catch(error=>console.log(error))
+    },[])
+  return (
+    <div className='tinderCards'>
+        <div className="tinderCards__cardsContainer">
+            {people.map((person, index)=>{
+                return <TinderCard
+                    className='swipe'
+                    key={index}
+                    preventSwipe={['up', 'down']}
+                >
+                    <div 
+                        className="card"
+                        style={{backgroundImage: `url(${person.url})`}}
+                    >
+                        <h3>{person.name}</h3>
+                    </div>
+                </TinderCard>
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default TinderCards
