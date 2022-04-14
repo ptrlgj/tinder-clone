d1f2327c2ec963cb0242d29d098441289cf14b95
Piotr Ligaj <piotr.ligaj24@gmail.com>
2022-04-14 16:34:39 +0200

    clear unused imports

--- src/pages/Chats.js
+++ src/pages/Chats.js
@@ -1,9 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import Chat from '../components/Chat'
 import { Link } from 'react-router-dom'
-import {database} from '../firebase'
-import {collection} from 'firebase/firestore'
-import { getDocs} from 'firebase/firestore'
 import { useGlobalContext } from '../contexts'
 import './Chats.scss'
 function Chats() {
